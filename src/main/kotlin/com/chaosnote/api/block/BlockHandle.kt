08955b9fb695de5799a2981fce1d6d930dabcf5b
Edgar <edgar.vahitov@gmail.com>
2026-01-25 02:21:08 +0200

    finish console plugin

--- src/main/kotlin/com/chaosnote/api/block/BlockHandle.kt
+++ src/main/kotlin/com/chaosnote/api/block/BlockHandle.kt
@@ -3,6 +3,7 @@ package com.chaosnote.api.block
 interface BlockHandle {
     val id: String
     val payload: String
-
     fun update(value: String)
+    fun saveState(state: Object)
+    fun getState(): Object?
 }
